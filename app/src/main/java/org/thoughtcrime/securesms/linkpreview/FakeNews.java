89e59cebdfc3c7750bdba948d10f4ff8f7357e90
Aedan Lawrence <Aedan@MrInterBugs.uk>
2021-08-29 05:14:59 +0100

    Added some JavaDoc.

--- app/src/main/java/org/thoughtcrime/securesms/linkpreview/FakeNews.java
+++ app/src/main/java/org/thoughtcrime/securesms/linkpreview/FakeNews.java
@@ -37,15 +37,14 @@ public class FakeNews {
    * @param url The URL to check.
    */
   public static void checkNews(@NonNull String url) {
-    String jsonUrl = "https://mrinterbugs.uk:5000/?article=" + url;
-
     try{
-      String tempResult = getResult(getJson(jsonUrl));
+      String tempResult = getResult(getJson(url));
 
       jsonObject = new JSONObject(tempResult);
       String ecdsaVerifyString = getSign();
 
-      Signature ecdsa = getECDSA();
+      Signature ecdsa = Signature.getInstance("SHA256withECDSA");
+
       X509EncodedKeySpec keySpec = getKeySpec();
       initialiseVerify(keySpec, ecdsa);
 
@@ -56,57 +55,110 @@ public class FakeNews {
     }
   }
 
+  /**
+   * Used to show the user and log the the result of the news check.
+   *
+   * @param result The final result on weather or not it is impostor news.
+   * @throws JSONException If it can not get the publisher from the JSON.
+   */
   private static void displayResult(boolean result) throws JSONException {
     SupportMapFragment mapFragment = new SupportMapFragment();
     Snackbar mySnackbar;
 
     if (result) {
       String valid = "The article just shared from the " + getPublisher() + " is defiantly not imposter news!";
-      mySnackbar = Snackbar.make(mapFragment.requireView(), valid, BaseTransientBottomBar.LENGTH_LONG);
       System.out.println(valid);
+      mySnackbar = Snackbar.make(mapFragment.requireView(), valid, BaseTransientBottomBar.LENGTH_LONG);
     } else {
       String invalid = "The article just shared from the " + getPublisher() + " can NOT be verified.";
-      mySnackbar = Snackbar.make(mapFragment.requireView(), invalid, BaseTransientBottomBar.LENGTH_LONG);
       System.out.println(invalid);
+      mySnackbar = Snackbar.make(mapFragment.requireView(), invalid, BaseTransientBottomBar.LENGTH_LONG);
     }
     mySnackbar.show();
   }
 
+  /**
+   * Used to return the publisher from the JSON result.
+   *
+   * @return The name of the publishing company.
+   * @throws JSONException Thrown if no publishing company can be found.
+   */
   private static String getPublisher() throws JSONException {
     return (String) jsonObject.get("Publisher");
   }
 
+  /**
+   * Checks the signature received from the verification using the public key.
+   *
+   * @param url the url of the news source.
+   * @param ecdsaVerifyString the verification string from the server.
+   * @param ecdsa the signature containing the public key.
+   * @return True or False depending on the verification.
+   * @throws SignatureException Required for both methods.
+   */
   private static boolean getFinalResult(String url, String ecdsaVerifyString, Signature ecdsa) throws SignatureException {
     ecdsa.update(url.getBytes(StandardCharsets.UTF_8));
     return ecdsa.verify(Base64.getDecoder().decode(ecdsaVerifyString));
   }
 
+  /**
+   * Used to start the verification process from the public key.
+   *
+   * @param keySpec The required keySpec for this encryption.
+   * @param ecdsa the signature to be verified.
+   * @throws NoSuchAlgorithmException thrown if the algorithm EC can not be found.
+   * @throws InvalidKeySpecException thrown if the keyFactory can not generate a valid key from the keySpec.
+   * @throws InvalidKeyException thrown if the key fails to initialise.
+   */
   private static void initialiseVerify(X509EncodedKeySpec keySpec, Signature ecdsa) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
     KeyFactory keyFactory = KeyFactory.getInstance("EC");
     ECPublicKey publicKey = (ECPublicKey) keyFactory.generatePublic(keySpec);
     ecdsa.initVerify(publicKey);
   }
 
+  /**
+   * Creates a keySpec for the public key.
+   *
+   * @return the said keySpec.
+   */
   private static X509EncodedKeySpec getKeySpec() {
     final String publicKeyPEM = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAELDIqPmr5Oxklns5GgKTLrxfS0WcKIjaCCW2ZsjBpwxcnQAItqUKSh5GCfj0tW6jVm4adiCCAKIDOBWhvIYqZ1Q==";
     byte[] encoded = Base64.getDecoder().decode(publicKeyPEM);
     return new X509EncodedKeySpec(encoded);
   }
 
-  private static Signature getECDSA() throws NoSuchAlgorithmException {
-    return Signature.getInstance("SHA256withECDSA");
-  }
-
+  /**
+   * Used to get the verification signature from the signing server.
+   *
+   * @return a String of the Base64 encoded signature.
+   * @throws JSONException thrown if the signature can't be found.
+   */
   private static String getSign() throws JSONException {
     return (String) jsonObject.get("Signature");
   }
 
+  /**
+   * Calls an AsyncTask to get the response from the signing server.
+   *
+   * @param finalUrl the URL to make a request to the signing server.
+   * @return the response from the server.
+   * @throws ExecutionException thrown if the response can't be got.
+   * @throws InterruptedException thrown if getting the response is interrupted.
+   */
   private static String getResult(URL finalUrl) throws ExecutionException, InterruptedException {
     AsyncTask<URL, Void, String> checkNewsURL = new CheckNewsURL().execute(finalUrl);
     return checkNewsURL.get();
   }
 
-  private static URL getJson(String jsonUrl) throws MalformedURLException {
+  /**
+   * Creates a URL to ping the verification server from the URL provided.
+   *
+   * @param url the news source to be checked.
+   * @return a new URL to be connected too.
+   * @throws MalformedURLException thrown if the string is not valid to make a URL from.
+   */
+  private static URL getJson(String url) throws MalformedURLException {
+    String jsonUrl = "https://mrinterbugs.uk:5000/?article=" + url;
     return new URL(jsonUrl);
   }
 }
